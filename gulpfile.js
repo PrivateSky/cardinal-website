02e5025eed02972e68b8c0d6d121085548a403bc
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2019-11-04 14:42:13 +0200

    fixed some font-awesome build-copy issues.

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ function buildWebComponents(cb) {
 
 function copyJsFile(cb) {
 
-    gulp.src('./node_modules/webcomponents/dist/cardinal.js')
+    gulp.src('node_modules/webcomponents/dist/cardinal.js')
         .pipe(include({
             separateInputs: true,
         }))
@@ -25,7 +25,7 @@ function copyJsFile(cb) {
 
 function copyJsFolderBuild(cb) {
 
-    gulp.src('./node_modules/webcomponents/dist/cardinal/**/*')
+    gulp.src('node_modules/webcomponents/dist/cardinal/**/*')
         .pipe(include({
             separateInputs: true,
         }))
@@ -34,7 +34,7 @@ function copyJsFolderBuild(cb) {
 };
 
 function copyThemes(cb) {
-    gulp.src('./node_modules/webcomponents/dist/themes/**/*')
+    gulp.src('node_modules/webcomponents/themes/**/*')
         .pipe(include({
             separateInputs: true,
         }))
@@ -45,14 +45,26 @@ function copyThemes(cb) {
 function copySourceFiles(cb) {
     gulp.src('src/**/*')
         .pipe(include({
-            separateInputs: true,
+            separateInputs: false,
         }))
         .pipe(gulp.dest('./release/'));
     cb();
 };
 
+function copyFontawesomeCSS(cb){
+    gulp.src('node_modules/@fortawesome/fontawesome-free/css/all.css')
+        .pipe(gulp.dest("./release/assets/css/fonts/fontawesome/css"));
+    cb();
+}
 
-exports.build = series(buildWebComponents, copyJsFile, copyJsFolderBuild, copyThemes, copySourceFiles);
+function copyFontawesomeFont(cb){
+    gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+        .pipe(gulp.dest("./release/assets/css/fonts/fontawesome/webfonts"));
+    cb();
+}
+
+
+exports.build = series(buildWebComponents, copyJsFile, copyJsFolderBuild, copyThemes, copySourceFiles, copyFontawesomeCSS, copyFontawesomeFont);
 
 
 
