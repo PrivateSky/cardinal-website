fe98fb9b54717350c9172fbbb55279d860ba3464
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2019-12-03 18:02:12 +0200

    bindig work

--- src/scripts/controllers/Controller.js
+++ src/scripts/controllers/Controller.js
@@ -1,4 +1,4 @@
-import { TENDER_CONTROLLER } from "../rivets/RivetsControllers.js";
+import {TENDER_CONTROLLER} from "../rivets/RivetsControllers.js";
 
 export default class Controller {
     constructor(element) {
@@ -17,6 +17,125 @@ export default class Controller {
 
     setState(newState) {
         this._state = newState;
+    }
+
+    set model(_model) {
+        let root = undefined;
+
+        function makeSetter(parentChain) {
+            return function (obj, prop, value) {
+                if (typeof value === "object") {
+                    obj[prop] = proxify(value, parentChain);
+                } else {
+                    obj[prop] = value;
+                }
+
+                root.notify(parentChain, prop);
+            }
+        }
+
+        function proxify(obj, parentChain) {
+            let isRoot = !parentChain;
+            let notify, onChange;
+
+            if (isRoot) {
+                let observers = {};
+                notify = function (parentChain, property) {
+
+                    let changedChain = parentChain + "." + property;
+
+                    function notifyChain(changedChain) {
+                        let chain = observers[changedChain];
+                        if (chain) {
+                            chain.forEach(callback => {
+                                callback();
+                            });
+                        }
+                    }
+
+                    notifyChain(changedChain);
+                    notifyChain("*");
+                };
+
+                onChange = function (chain, callback) {
+                    if (typeof callback === "function") {
+                        if (!observers[chain]) {
+                            observers[chain] = [];
+                        }
+                        observers[chain].push(callback);
+                    }
+                }
+            }
+            let setter = makeSetter(parentChain);
+
+            return new Proxy(obj, {
+                get: function (obj, prop) {
+                    if(isRoot){
+                        switch (prop) {
+                            case "onChange":
+                                return onChange;
+                            case "notify":
+                                return notify;
+                        }
+                    }
+
+                    return obj[prop];
+                },
+                set: makeSetter(parentChain),
+
+                deleteProperty: function (oTarget, sKey) {
+                    delete oTarget[sKey];
+                },
+
+                ownKeys: function (oTarget) {
+                    return oTarget.keys();
+                },
+                has: function (oTarget, sKey) {
+                    return sKey in oTarget
+                },
+                defineProperty: function (oTarget, sKey, oDesc) {
+                    let oDescClone = Object.assign({}, oDesc);
+                    oDescClone.set = function (obj, prop, value) {
+                        if (oDesc.hasOwnProperty("set")) {
+                            oDesc.set(obj, prop, value);
+                        }
+                        setter(obj, prop, value);
+                    };
+                    return Object.defineProperty(oTarget, sKey, oDescClone);
+                },
+                getOwnPropertyDescriptor: function (oTarget, sKey) {
+                    return Object.getOwnPropertyDescriptor(oTarget, sKey)
+                }
+            });
+        }
+
+        function proxifyArray(arr, parentChain) {
+            new Proxy([], {
+                get(target, prop) {
+                    const val = target[prop];
+                    if (typeof val === 'function') {
+                        if (['push', 'unshift'].includes(prop)) {
+                            return function (el) {
+                                console.log('this is a array modification');
+                                return Array.prototype[prop].apply(target, arguments);
+                            }
+                        }
+                        if (['pop'].includes(prop)) {
+                            return function () {
+                                const el = Array.prototype[prop].apply(target, arguments);
+                                console.log('this is a array modification');
+                                return el;
+                            }
+                        }
+                        return val.bind(target);
+                    }
+                    return val;
+                }
+            });
+        }
+
+        root = proxify(_model);
+        this.model = root;
     }
 
     getState() {
