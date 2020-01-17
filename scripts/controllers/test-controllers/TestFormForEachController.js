ab15644aea0500b642ec5189109c956e7f3d7493
paiusCatalin <paiusCatalin@users.noreply.github.com>
2020-01-17 13:42:24 +0000

    Deploying to gh-pages from master ef845a6ad1e96d0ca7d23e8535a28e060faef8a1

--- scripts/controllers/test-controllers/TestFormForEachController.js
+++ scripts/controllers/test-controllers/TestFormForEachController.js
@@ -2,9 +2,7 @@ import DefaultFormController from "../DefaultFormController.js";
 
 export default class TestFormForEachController extends DefaultFormController {
     constructor(element) {
-        super(element);
-
-        this.model = this.setModel({
+        super(element, {
             entities: [{
                 sectionTitle: {
                     label: "Your personal information"
