cf84c074916ef145c42b3f77c0850af9d4aca045
paiusCatalin <paiusCatalin@users.noreply.github.com>
2020-01-24 13:57:17 +0000

    Deploying to gh-pages from master - 0ac5b3348deda53f73011e7f24c9dfcd1e20e3b0 🚀

--- scripts/controllers/DefaultFormController.js
+++ scripts/controllers/DefaultFormController.js
@@ -1,4 +1,5 @@
 import BindableController from "./BindableController.js";
+// import FormUtils from "../utils/FormValidator.js";
 
 export default class DefaultFormController extends BindableController {
     constructor(element, model) {
@@ -15,10 +16,28 @@ export default class DefaultFormController extends BindableController {
 
     __initDefaultFormListeners() {
         this._element.addEventListener('submit', (event) => {
+            console.log('{FormController.js} Form is listening for -=submit=-');
             event.preventDefault();
             event.stopImmediatePropagation();
 
-            console.log('{FormController.js} Form is listening for -=submit=-');
+            // let formUtils = new FormUtils(this.model);
+            // formUtils.createFormData();
+
+            // ,
+            // {
+            //     "name": "Forms",
+            //     "children": [{
+            //         "name": "using forms"
+            //     }]
+            // }
+
+            // console.log(`The form is ${formUtils.isValidForm() ? 'valid' : 'invalid'}`);
+            // console.log('[Submit handler] -- model --');
+            // console.log(this.model);
+            // console.log('[Submit handler] -- model --');
+            // console.log('[Submit handler] -- formData --');
+            // console.log(formUtils.getFormData());
+            // console.log('[Submit handler] -- formData --');
         }, true);
 
         this._element.addEventListener('reset', (event) => {
