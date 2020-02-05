1aba2365512347f4efe718fc6e84088a9b879162
Mastaleru <mastaleru.rafael@gmail.com>
2020-02-05 14:04:49 +0000

    Deploying to gh-pages from master - 676e5c0ec5d8261b09c648e9051d64a8675a3a9b 🚀

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ import DefaultApplicationController from "./controllers/base-controllers/Default
 import TestFormController from "./controllers/test-controllers/TestFormController.js";
 import TestFormForEachController from "./controllers/test-controllers/TestFormForEachController.js";
 import TestFormChildrenController from './controllers/test-controllers/TestFormChildrenController.js';
+import RenderListController from './controllers/test-controllers/RenderListController.js';
 import ApplicationController from "./controllers/base-controllers/ApplicationController.js";
 import DefaultFormController from "./controllers/base-controllers/DefaultFormController.js";
 
@@ -11,6 +12,7 @@ document.addEventListener("controllerFactoryIsReady", (e) => {
     ControllerFactory.registerController("TestFormController", TestFormController);
     ControllerFactory.registerController("ApplicationController", ApplicationController);
     ControllerFactory.registerController("DefaultFormController", DefaultFormController);
+    ControllerFactory.registerController("RenderListController", RenderListController);
     ControllerFactory.registerController("TestFormForEachController", TestFormForEachController);
     ControllerFactory.registerController("TestFormChildrenController", TestFormChildrenController);
     ControllerFactory.registerController("DefaultApplicationController", DefaultApplicationController);
